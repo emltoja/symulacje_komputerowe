57bed6d3c5a59e360b0c7e28c91f5b2a8093fa72
Emil Olszewski <268789@student.pwr.edu.pl>
2023-04-04 22:14:02 +0200

    dokumentacja i formatowanie

--- lista3/AcceptReject.jl
+++ lista3/AcceptReject.jl
@@ -1,47 +1,71 @@
-
+"""Moduł zawierający funkcje do generowania zmiennych losowych metodą akceptacji-odrzucenia"""
 module AcceptReject
     
     using Distributions
-
     export accept_reject
 
+
+    """
+    Generuje liczby z rozkładu `destination_dist` metodą akceptacji-odrzucenia.
+    """
     function accept_reject(sampled_dist :: Distribution, destination_dist :: Distribution, c, N; mult=1)
 
         samples = Vector{Float64}(undef, N)
+
         prev_filled_count = 0
-        filled_count = 0
-        size = N
+        filled_count      = 0
+        size              = N
 
         while filled_count < N
+            
             x = rand(sampled_dist, size)
+            
             mask = c .* rand(size) .* pdf.(sampled_dist, x) .<= mult .* pdf.(destination_dist, x)
-            filled_now = sum(mask)
+            
+            filled_now    = sum(mask)
             filled_count += filled_now
-            size -= filled_now
+            size         -= filled_now
+            
             samples[1+prev_filled_count:filled_count] = x[mask][1:filled_count-prev_filled_count]
             prev_filled_count = filled_count
+        
         end
-        samples
+        
+        return samples
+    
     end
 
+
+    """
+    Generuje liczby z rozkładu o funkcji gęstości `destination_pdf` metodą akceptacji-odrzucenia.
+    """
     function accept_reject(sampled_dist :: Distribution, destination_pdf :: Function, c, N; mult=1)
 
         samples = Vector{Float64}(undef, N)
-        prev_filled_count = 0
-        filled_count = 0
-        size = N
 
+        prev_filled_count = 0
+        filled_count      = 0
+        size              = N
 
         while filled_count < N
+        
             x = rand(sampled_dist, size)
+            
+            # Wektor wskazujący na elementy do zaakceptowania 
             mask = c .* rand(size) .* pdf.(sampled_dist, x) .<= mult .* destination_pdf.(x)
-            filled_now = sum(mask)
+
+            filled_now    = sum(mask)
             filled_count += filled_now
-            size -= filled_now
+            size         -= filled_now
+
+
             samples[1+prev_filled_count:filled_count] = x[mask][1:filled_count-prev_filled_count]
             prev_filled_count = filled_count
+
         end
-        samples
+
+        return samples
+
     end
 
 end
