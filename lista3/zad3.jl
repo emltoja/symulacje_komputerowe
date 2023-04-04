0302d1d6168e97894e702a13a71118808b91b0d6
Emil Olszewski <268789@student.pwr.edu.pl>
2023-04-04 19:57:32 +0200

    zmiana |N(0, 1)| na N(0, 1)

--- lista3/zad3.jl
+++ lista3/zad3.jl
@@ -3,6 +3,6 @@ using Plots
 using .AcceptReject
 using Distributions
 
-samples = accept_reject(Exponential(1), Normal(0, 1), sqrt(2ℯ/π), 1_000_000; mult=2)
-
+samples = accept_reject(Exponential(1), Normal(0, 1), sqrt(2ℯ/π), 1_000_00; mult=2)
+samples = vcat(samples, -1 .* samples)
 histogram(samples, normalize=:pdf, label=false)
