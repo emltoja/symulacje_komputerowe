5f8cc8745b0a8036a3836e4d9aaa1bd5f08f2633
Emil Olszewski <268789@student.pwr.edu.pl>
2023-04-18 13:38:51 +0200

    wektoryzacja

--- lista4/zad1.jl
+++ lista4/zad1.jl
@@ -4,18 +4,31 @@ using Distributions
 using HypothesisTests
 
 # Generowanie zmiennych losowych 
-# TODO: Wektoryzacja
-function generate(h :: Function, xrange :: Int, yrange :: Int)
+function generate(h :: Function, xrange :: Int, yrange :: Int, size :: Int)
 
-    x = xrange * rand()                 # x ∈ [0, xrange]
-    y = 2yrange * rand() - yrange       # y ∈ [-yrange, yrange]
+    samples = Vector{Float64}(undef, size)
+
+    prev_filled_count = 0
+    filled_count = 0
+    N = size
     
-    if x^2 <= h(y/x)
-        return y/x
-    else 
-        generate(h, xrange, yrange)
+    while filled_count < size
+       
+        x = xrange .* rand(N)                 # x ∈ [0, xrange]
+        y = 2yrange .* rand(N) .- yrange       # y ∈ [-yrange, yrange]
+        z = y ./ x
+       
+        mask = x.^2 .<= h.(z)
 
+        filled_now = sum(mask)
+        filled_count += filled_now
+        N -= filled_now
+
+        samples[1+prev_filled_count:filled_count] = z[mask][1:filled_count-prev_filled_count]
+        prev_filled_count = filled_count
     end
+
+    return samples
 end
 
 cauchy(x) = 1/(1 + x^2)
@@ -26,7 +39,7 @@ normal(x) =ℯ^(-x^2/2)
 # --------------
 
 # Wygenerowanie próbki z rozkładu N(0, 1)
-normal_samples = [generate(normal, 1, 1) for _ in 1:1000000]
+normal_samples = generate(normal, 1, 1, 10000)
 
 # Gęstości
 histogram(normal_samples, normalize=:pdf, label=false)
@@ -38,7 +51,7 @@ ExactOneSampleKSTest(normal_samples, Normal(0, 1))
 
 
 # Wygenerowanie próbki z rozkładu C(0, 1)
-cauchy_samples = [generate(cauchy, 1, 1) for _ in 1:1000000]
+cauchy_samples = generate(cauchy, 1, 1, 10000)
 
 # Gęstości
 cauchy_xs = LinRange(-10, 10, 200)
