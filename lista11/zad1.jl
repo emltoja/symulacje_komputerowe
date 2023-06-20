f5ac08908720aabf23c51b11961fd61b200c968d
Emil Olszewski <268789@student.pwr.edu.pl>
2023-06-20 15:07:30 +0200

    komentarze i usprawnione wykresy

--- lista11/zad1.jl
+++ lista11/zad1.jl
@@ -2,13 +2,14 @@
 # Wielowymiarowy ruch Browna
 using Plots
 
+# Trajektoria ruchu Browna
 function brown(times, dim)
 
     Δts = diff(times)
-    pushfirst!(Δts, times[1])
+    pushfirst!(Δts, 0)
 
+    # Przyrosty z rozkładu N(0, t)
     incs = randn(length(times), dim)
-
     for i in 1:dim
         incs[:, i] .*= sqrt.(Δts)
     end
@@ -17,12 +18,19 @@ function brown(times, dim)
 
 end
 
-
-function plot_brown(times, n)
-    plot(eachcol(brown(times, n))...)
+# Wykres trajektorii ruchu Browna
+function plot_brown(times, n; kwargs...)
+    if n > 1
+        plot(eachcol(brown(times, n))...; kwargs..., aspect_ratio=1)
+        scatter!(eachcol(zeros(1, n))..., markersize=3, color=:red, label="Punkt początkowy")
+    else 
+        plot(times, brown(times, n); kwargs...)
+    end
 end
 
-
-plot_brown(1:0.1:100, 1)
-plot_brown(1:0.1:100, 2)
-plot_brown(1:0.1:100, 3)
+# Jednowymiarowy ruch Browna
+plot_brown(1:0.1:100, 1, label=nothing, title="Jednowymiarowy ruch Browna")
+# Dwuwymiarowy 
+plot_brown(1:0.1:100, 2, label=nothing, title="Dwuwymiarowy ruch Browna")
+# Trójwymiarowy
+plot_brown(1:0.1:100, 3, label=nothing, title="Trójwymiarowy ruch Browna")
